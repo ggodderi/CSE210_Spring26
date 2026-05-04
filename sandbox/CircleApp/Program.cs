af179716f869fd5d9b6e9e75d1b45bcffb112175
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2026-05-04 14:44:28 -0600

    Create Circle App.

---
+++ sandbox/CircleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello world");
+        Circle myCircle = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine(myCircle.GetArea());
+        
+    }
+}
---
+++ sandbox/Quizzes/C#3-5Quiz/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+
+    static double  FindSmallest(List<double> data)
+    {
+        double smallest = data[0];
+        foreach (double d in data)
+        {
+            if (d < smallest)
+            {
+                smallest = d;
+            }
+        }
+        return smallest;
+    }
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Hey");
+        List<double> data = new List<double> {9.58, 9.69, 9.72, 9.74, 9.78 };
+
+        Console.WriteLine(FindSmallest(data));
+    }
+}
