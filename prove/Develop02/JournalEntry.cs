3b5358e4dd35b99bf74b62e163c924ece3239d27
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2026-05-13 14:32:38 -0600

    Added the initial JournalEntry Class.

---
+++ prove/Develop02/JournalEntry.cs
@@ -0,0 +1,25 @@
+class JournalEntry
+{
+    string _date;
+    string _prompt;
+    string _response;
+
+    string[] _prompts =
+    {
+        "How are you feeling today",
+        "Who did you talk with today"
+    };
+
+    public void CreateJournalEntry()
+    {
+        _date = DateTime.Now.ToShortDateString();
+        _prompt = _prompts[0];
+        Console.WriteLine(_prompt);
+        _response = Console.ReadLine();
+    }
+
+    public void DisplayJournalEntry()
+    {
+        Console.WriteLine($"{_date}, {_prompt}, {_response}");
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        JournalEntry myJournalEntry = new JournalEntry();
+        myJournalEntry.CreateJournalEntry();
+        myJournalEntry.DisplayJournalEntry();
     }
 }
